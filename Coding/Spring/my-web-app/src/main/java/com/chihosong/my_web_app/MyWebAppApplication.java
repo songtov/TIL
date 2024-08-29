b98648a8588bb9512caa0889593e3aec9eec89f0
DevSong7 <chiho.song@angelswing.io>
2024-08-29 23:14:13 +0900

    new spring-boot project

---
+++ Coding/Spring/my-web-app/src/main/java/com/chihosong/my_web_app/MyWebAppApplication.java
@@ -0,0 +1,22 @@
+package com.chihosong.my_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAppApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ Coding/Spring/my-web-app/src/test/java/com/chihosong/my_web_app/MyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chihosong.my_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
